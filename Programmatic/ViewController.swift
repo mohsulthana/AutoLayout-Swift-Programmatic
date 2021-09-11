dc5bf24f896d31ffe8290b664604e6273e467e9a
Mohammad Sulthan <37440405+mohsulthana@users.noreply.github.com>
2021-09-11 15:42:57 +0700

    Initial Commit

---
+++ Programmatic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Programmatic
+//
+//  Created by Mohammad Sulthan on 11/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
